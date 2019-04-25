24c3f45c569098738c80b0d9b8ce3ef41b7e6fe1
quarr <quarr@SnorgleLaptop3.gateway>
2019-04-26 00:57:19 +1000

    Fixed up some comments.

--- Assignment-1/src/rotationencrypt.c
+++ Assignment-1/src/rotationencrypt.c
@@ -18,45 +18,41 @@ int rotationEncrypt (void)
 
 	int b=0, c=0, key=10;
 
-	char textstring [1024];// = "AZ MY FIRST TEST STRING" ;
-
-	//puts("ENGG1003_Assessment 1_Rotation Encryption"); /* prints ENGG1003_Assessment 1_Rotation Encryption */
+	char textstring [1024];	//Declaration for text string array
 
 	rot_input_file = fopen("rotation_text.txt", "r");	//Open the file in read mode
-		fgets(textstring, 1024,(FILE*)rot_input_file);			//Read the text string from file
-		fscanf(rot_input_file, "%d", &key);						//Read the key from file
-		fclose(rot_input_file);									//Close the file
+		fgets(textstring, 1024,(FILE*)rot_input_file);	//Read the text string from file
+		fscanf(rot_input_file, "%d", &key);				//Read the key from file
+		fclose(rot_input_file);							//Close the file
 
 		if (key <0 || key >26) {					//Check the input key is valid number range
-			//system("cls");
 			printf("\nKey value was invalid\n");	//Print to screen error message
-			return 0;							//Turn around and do it again, back to the rotation menu
+			return 0;								//Turn around and do it again, back to the rotation menu
 		}
 
-
 	printf("Text to encrypt: %s", textstring);  //Print the text string.
-	printf("Encryption key: %d\n", key);  //Print the text string.
+	printf("Encryption key: %d\n", key);  		//Print the key.
 
-	for (int a=0 ; textstring[a] != '\0' ; a++ ) // Run a loop and encrypt the string.
+	for (int a=0 ; textstring[a] != '\0' ; a++ ) // Run a loop and encrypt the string until the NULL at the end of the string is reached.
 	{
-		b = textstring[a];
-		if (b >= 65 && b <= 90) //Only encrypt the capital letters.
+		b = textstring[a];			//b is assigned the integer value of the letter at that index
+		if (b >= 65 && b <= 90) 	//Only encrypt the capital letters.
 		{
-			c=b+key;
-			if (c>90) // Subtraction to keep the result within capital letters
+			c=b+key;	//Add the key value to the letter value
+			if (c>90) 	//If the added value goes higher than upper case Z, subtraction required
 			{
-				c=c-26;
+				c=c-26;	// Subtraction to keep the result within capital letters
 			}
-			//printf("Value at index %d is: %d plus key value: %d\n", a, b, c); //Check the internal process
-			textstring[a]=c; // Change the value of the letter
+
+			textstring[a]=c; //Assign the new encrypted value of the letter
 		}
 	}
 
 	rot_output_file = fopen("rotation_text.txt", "w");	//Open the file in write mode
-	fputs(textstring, rot_output_file);						//Write the text string to file
-	fputs("\n", rot_output_file);							//Add a new line character
-	fprintf(rot_output_file, "%d", key);						//Write the manual output key value to the file
-	fclose(rot_output_file);									//Close the file
+	fputs(textstring, rot_output_file);					//Write the encrypted text string to the file
+	fputs("\n", rot_output_file);						//Add a new line character
+	fprintf(rot_output_file, "%d", key);				//Write the manual output key value to the file
+	fclose(rot_output_file);							//Close the file
 
 	/*
 	rot_start:
